bf62572121e8ba0bae47460bea80b1aa0d95ab8b
solicit-snake <kyle.vinc11@gmail.com>
2024-11-01 12:32:34 +1030

    doing some cimmt
    dwadwa

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Box from './components/Box'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function App() {
+
+  return (
+    <div className="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <Box/>
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Box.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { BoxGeometry } from 'three'
+import { useRef } from 'react'
+
+import React from 'react'
+
+export default function Box(props) {
+    const boxRef = useRef();
+
+    useFrame((_, delta) => {
+        boxRef.current.rotation.x += 1 * delta
+        boxRef.current.rotation.y += 0.5 * delta
+    }, [])
+
+    return (
+        <mesh {...props} ref={boxRef}>
+            <boxGeometry />
+            <meshBasicMaterial color={0x00ff00} wireframe />
+        </mesh>
+    )
+}
